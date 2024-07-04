1cc4987738e382859e7e4d0d694ed455f5f8eea2
Srishti39 <142647400+Srishti39@users.noreply.github.com>
2024-07-04 21:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navButtons = document.querySelector('.nav-buttons');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navButtons.classList.toggle('active');
+});
